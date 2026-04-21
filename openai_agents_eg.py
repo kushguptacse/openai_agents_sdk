79fee76efc2d55ce04034e16dfe667498aed0aaa
Kush Gupta <kushguptacse@gmail.com>
2026-04-21 09:50:46 +0000

    Enhance agent initialization by adding model settings for reasoning effort

--- openai_agents_eg.py
+++ openai_agents_eg.py
@@ -1,4 +1,4 @@
-from agents import Agent, Runner, OpenAIChatCompletionsModel, AsyncOpenAI
+from agents import Agent, ModelSettings, Runner, OpenAIChatCompletionsModel, AsyncOpenAI
 from config import API_URL, CHAT_COMPLETIONS_API_KEY, LLM_MODEL_NAME
 from agents import set_tracing_disabled
 import asyncio
@@ -9,13 +9,14 @@ qwen3_client = AsyncOpenAI(base_url=API_URL, api_key=CHAT_COMPLETIONS_API_KEY)
 
 model = OpenAIChatCompletionsModel(
     model=LLM_MODEL_NAME, 
-    openai_client=qwen3_client
+    openai_client=qwen3_client,
 )
 
 agent = Agent(
     name="Assistant",
     instructions="You are a helpful assistant",
-    model=model
+    model=model,
+    model_settings=ModelSettings(extra_body={"reasoning_effort": "none"})
 )
 
 
