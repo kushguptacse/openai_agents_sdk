cad991e1c7510936531a5ed9675d8b55e5201af3
Kush Gupta <kushguptacse@gmail.com>
2026-04-27 06:49:28 +0000

    refactoring done on openai_agent_guardrail

--- openai_agent_guardrail.py
+++ openai_agent_guardrail.py
@@ -5,11 +5,14 @@ from custom_agent_qwen3 import get_model
 from system_prompt import name_check_gaurdrail_instructions
 from dotenv import load_dotenv
 
-load_dotenv(override=True) # Load environment variables from .env file, override existing ones if any
+load_dotenv(
+    override=True
+)  # Load environment variables from .env file, override existing ones if any
 
 
 model, model_settings = get_model()
-#model = "gpt-4o-mini"
+# model = "gpt-4o-mini"
+
 
 class NameCheckOutput(BaseModel):
     is_name_in_message: bool
@@ -21,16 +24,19 @@ guardrail_agent = Agent(
     instructions=name_check_gaurdrail_instructions,
     model=model,
     model_settings=model_settings,
-    output_type=NameCheckOutput #specifies that the output of this agent should be parsed into a NameCheckOutput object
+    output_type=NameCheckOutput,  # specifies that the output of this agent should be parsed into a NameCheckOutput object
 )
-
 
 
 @input_guardrail
 async def guardrail_against_name(ctx, agent, message):
     result = await Runner.run(guardrail_agent, message, context=ctx.context)
     is_name_in_message = result.final_output.is_name_in_message
-    return GuardrailFunctionOutput(output_info={"found_name": result.final_output},tripwire_triggered=is_name_in_message)
+    return GuardrailFunctionOutput(
+        output_info={"found_name": result.final_output},
+        tripwire_triggered=is_name_in_message,
+    )
+
 
 async def main():
 
@@ -39,11 +45,11 @@ async def main():
         instructions="You are a test agent that generates a message",
         model=model,
         model_settings=model_settings,
-        input_guardrails=[guardrail_against_name] #attach the guardrail to this agent
+        input_guardrails=[guardrail_against_name],  # attach the guardrail to this agent
     )
     result = await Runner.run(test_agent, "Generate a message 'hello' from alice")
     print(f"Generated message: {result}")
-    
+
 
 if __name__ == "__main__":
     asyncio.run(main())
